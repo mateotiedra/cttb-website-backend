81d5088b95e1c1c9d598b9ef1ffa71700fedfeab
mateotiedra <mateo@tiedra.com>
2022-04-28 20:44:28 +0200

    add get all user funcrtion

--- app/controllers/user.controller.js
+++ app/controllers/user.controller.js
@@ -7,17 +7,41 @@ const {
 const User = db.user;
 const Op = db.Sequelize.Op;
 
-const getUserBoard = (req, res) => {
-  const attributesToSend = ['email', 'status', 'role'];
-  let userData = {};
+const blackListAttributes = [
+  'uuid',
+  'emailToken',
+  'emailTokenGeneratedAt',
+  'password',
+];
+
+const filterUserAttributes = (user) => {
+  const attributesToSend = Object.keys(User.rawAttributes).filter(
+    (attribute) => !blackListAttributes.includes(attribute)
+  );
+
+  let userSafeData = {};
 
   attributesToSend.forEach((attribute) => {
-    userData[attribute] = req.user[attribute];
+    userSafeData[attribute] = user[attribute];
   });
 
+  return userSafeData;
+};
+
+const getUserBoard = (req, res) => {
+  const userData = filterUserAttributes(req.user);
   return res.status(200).json(userData);
+};
+
+const getEveryUserBoard = (req, res) => {
+  User.findAll({ attributes: { exclude: blackListAttributes } })
+    .then((users) => {
+      res.status(200).json(users);
+    })
+    .catch(unexpectedErrorCatch(res));
 };
 
 module.exports = {
   getUserBoard,
+  getEveryUserBoard,
 };
--- app/routes/user.routes.js
+++ app/routes/user.routes.js
@@ -1,8 +1,18 @@
-const { verifyAccessToken } = require('../middlewares/user.middleware');
+const {
+  verifyAccessToken,
+  verifyRole,
+} = require('../middlewares/user.middleware');
 const { verifyRequestBody } = require('../middlewares/request.middleware');
 const controller = require('../controllers/user.controller');
 
 module.exports = function (app) {
   // Get the user's basics infos
-  app.get('/auth/u', [verifyAccessToken], controller.getUserBoard);
+  app.get('/user/u', [verifyAccessToken], controller.getUserBoard);
+
+  // Get all the users
+  app.get(
+    '/user/all',
+    [verifyAccessToken, verifyRole(['admin'])],
+    controller.getEveryUserBoard
+  );
 };
